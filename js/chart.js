ec63e38cc7cc9b49be55f30188e252420606da0c
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2019-05-01 09:51:14 -0300

    troca bar chart para line chart

--- js/chart.js
+++ js/chart.js
@@ -190,36 +190,72 @@ new Chart(ctx, {
 });
 
 var ctx = document.getElementById('visao-chart').getContext('2d');
+// new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//       labels: ["10", "11", "12", "13", "14", "15", "16"],
+//       datasets: [
+//         {
+//           label: "DOR",
+//           backgroundColor: DEFAULT_COLORS[4],
+//           data: [100,100,100,100,100,100,100]
+//         }, {
+//           label: "Mucosite",
+//           backgroundColor: DEFAULT_COLORS[4],
+//           data: [150,150,150,150,150,150,150]
+//         }, {
+//           label: "Diarreia",
+//           backgroundColor: DEFAULT_COLORS[4],
+//           data: [50,50,50,50,50,50,50]
+//         }
+//       ]
+//     },
+//     options: {
+//       title: {
+//         display: false
+// 			},
+// 			scales: {				
+// 				yAxes: [{
+// 					ticks: {
+// 						beginAtZero: true
+// 					}
+// 				}]
+// 			}
+//     }
+// });
+
 new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["10", "11", "12", "13", "14", "15", "16"],
-      datasets: [
-        {
-          label: "DOR",
-          backgroundColor: DEFAULT_COLORS[4],
-          data: [100,100,100,100,100,100,100]
-        }, {
-          label: "Mucosite",
-          backgroundColor: DEFAULT_COLORS[4],
-          data: [150,150,150,150,150,150,150]
-        }, {
-          label: "Diarreia",
-          backgroundColor: DEFAULT_COLORS[4],
-          data: [50,50,50,50,50,50,50]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: false
-			},
-			scales: {				
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
-					}
-				}]
-			}
-    }
+  type: 'line',
+  data: {
+    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    datasets: [{ 
+        data: [10,2,30,40,50,60,70,80,90,100,110,125,136,148,159,167,17,189,109,200,211,222,233,244,255,266,279,298,329,330],
+        label: "DOR",
+        borderColor: DEFAULT_COLORS[4],
+        fill: false
+      }, { 
+        data: [11,21,31,41,51,61,7,8,9,100,101,120,139,144,145,146,170,181,191,210,212,223,234,245,250,256,257,258,259,350],
+        label: "Mucosite",
+        borderColor: DEFAULT_COLORS[3],
+        fill: false
+      }, { 
+        data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+        label: "Diarreia",
+        borderColor: DEFAULT_COLORS[2],
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: false
+		},
+		scales: {				
+							yAxes: [{
+								ticks: {
+									beginAtZero: true
+								}
+							}]
+						}
+  }
 });
