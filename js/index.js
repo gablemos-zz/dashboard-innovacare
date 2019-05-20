22381318aadfd0eaf401e96eed202ccbbcd0b6de
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2019-05-20 19:45:00 -0300

    ps

--- js/index.js
+++ js/index.js
@@ -69,64 +69,52 @@ $(document).ready(function() {
         },
         "lengthMenu": [[10], [10]]
     } );
-// } );
+  } );
 
-// $(document).ready(function() {
-//     $('#datatable-agenda').DataTable( {
-//         "language": {
-//             "decimal":        "",
-//             "emptyTable":     "Nenhum registro",
-//             "info":           "Exibindo _START_ até _END_ de _TOTAL_ registros",
-//             "infoEmpty":      "Exibindo 0 até 0 de 0 registros",
-//             "infoFiltered":   "(filtrado do _MAX_ total registros)",
-//             "infoPostFix":    "",
-//             "thousands":      ",",
-//             "lengthMenu":     "_MENU_",
-//             "loadingRecords": "Carregando...",
-//             "processing":     "Processando...",
-//             "search":         "Buscar:",
-//             "zeroRecords":    "Nenhum registro",
-//             "paginate": {
-//                 "first":      "Primeiro",
-//                 "last":       "Último",
-//                 "next":       "Próximo",
-//                 "previous":   "Anterior"
-//             },
-//             "aria": {
-//                 "sortAscending":  ": ative para ordenar coluna crescente",
-//                 "sortDescending": ": ative para ordenar coluna decrescente"
-//             }
-//         },
-//         "lengthMenu": [[5], [5]]
-//     } );
-// } );
-
-// $(document).ready(function() {
-//     $('#datatable-paciente').DataTable( {
-//         "language": {
-//             "decimal":        "",
-//             "emptyTable":     "Nenhum registro",
-//             "info":           "Exibindo _START_ até _END_ de _TOTAL_ registros",
-//             "infoEmpty":      "Exibindo 0 até 0 de 0 registros",
-//             "infoFiltered":   "(filtrado do _MAX_ total registros)",
-//             "infoPostFix":    "",
-//             "thousands":      ",",
-//             "lengthMenu":     "_MENU_",
-//             "loadingRecords": "Carregando...",
-//             "processing":     "Processando...",
-//             "search":         "Buscar:",
-//             "zeroRecords":    "Nenhum registro",
-//             "paginate": {
-//                 "first":      "Primeiro",
-//                 "last":       "Último",
-//                 "next":       "Próximo",
-//                 "previous":   "Anterior"
-//             },
-//             "aria": {
-//                 "sortAscending":  ": ative para ordenar coluna crescente",
-//                 "sortDescending": ": ative para ordenar coluna decrescente"
-//             }
-//         },
-//         "lengthMenu": [[5], [5]]
-//     } );
-// } );
+  $(function()
+  {
+    $('#form').validate(
+      {
+      rules:
+        {
+            PlanoTEncaminhadoPS:{ required:true },
+          other:
+          {
+            required:
+            {
+            depends:function()
+            {
+              var sel = $('input[name=channel]:checked', '#form').val();
+              
+              if(sel =='other')
+                {
+                return true;
+                }
+              else
+                {
+                return false;
+                }
+            }
+            }
+          }
+        },
+        messages:
+        {
+            PlanoTEncaminhadoPS:
+          {
+            required:"Please select an option"
+          }
+        },
+        errorPlacement: function(error, element) {
+            if ( element.is(":radio") ) 
+            {
+                error.appendTo( element.parents('.container') );
+            }
+            else 
+            { // This is the default behavior 
+                error.insertAfter( element );
+            }
+         }
+      });
+    
+  });
